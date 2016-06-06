b57c7f16ec5d9e6c33fb5a10275369e404a02094
UMSF-K13-2-Gnezdilova <juliagnezdilova4@gmail.com>
2016-06-06 16:55:04 +0300

    Commite

---
+++ Gnezdilova/src/world.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class world {
+
+	public static void main(String[] args) {
+		Random r = new Random();
+		int q[]=new int [10];
+        for (int i = 0; i<10;i++) 
+        {
+            q[i]=r.nextInt(20);
+        System.out.println(q[i]);
+        }
+            int min=q[0];
+        
+           
+        for(int x: q) {
+                if(x < min) min = x;
+        }
+        System.out.println("Minimal element: " + min);
+
+	}
+}
+
+
